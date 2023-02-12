03c9d81123ba5882b1bab5e5a8bf1fd250c0a8cc
Martin Reyes <martinreyesrf@gmail.com>
2023-02-12 00:49:57 -0600

    modificacion para sea en bucle y sirva para windows

--- main.py
+++ main.py
@@ -1,52 +1,39 @@
 from pytube import YouTube
 import os
+import glob
 import shutil
 
-Urls = []
-ruta = './audioMp4'
-
-def AgregarALista(input):
-    Urls.append(input)
-
-def Menu():
-    print(Urls)
+def menu():
+    os.system('cls')
     print("Youtube Url:")
-    UrlInput = str(input())
-    print("1) Agregar ")
-    print("2) Descargar ")
-    inputMenu = str(input('Ingrese:'))
-    AgregarALista(UrlInput)
-    if inputMenu == '1':
-        Menu()
-    if inputMenu == '2':
-        Descargar()
-def Descargar():
+    url = str(input())
+    descargar(url)
+
+def descargar(url):
     print('Iniciando descargas')
-    for url in Urls:
-        yt = YouTube(url).streams.filter(abr="128kbps",type="audio").first()
-        donwloadFileMp4Audio = yt.download(output_path=ruta)
-        print("Done: " + YouTube(url).title)
-    Convertir()
-def Convertir():
-    print('Convertir a mp3')
-    listMp4 = os.listdir(path=ruta)
-    comandoEntrarCaparteMp4 = 'cd ' + ruta + "\n"
-    for itemMp4 in listMp4:
-        itemMp3 = itemMp4.replace(".mp4",".mp3")
-        comandoConvertir = "ffmpeg -i '" + itemMp4 + "' '" + itemMp3 +"'\n"
-        os.system(comandoEntrarCaparteMp4 + comandoConvertir)
-        os.system(comandoEntrarCaparteMp4 + "rm '" + itemMp4 + "'")
-    print('Listo')
-    Mover()
-def Mover():
-    print('Moviendo')
-    listMp3 =  os.listdir(path=ruta)
-    for mp3 in listMp3:
-        fileMp3Route = "./audioMp4/" + mp3
-        shutil.move(fileMp3Route, 'mp3/')
-    print('TODO CORRECTO, files listos /mp3/')
+    yt = YouTube(url).streams.filter(abr="128kbps",type="audio").first()
+    yt.download()
+    print("Done: " + YouTube(url).title)
+    convertir()
 
+def convertir():
+    ruta = os.path.join(os.path.dirname(__file__), "*.mp4")
+    mp4 = glob.glob(ruta)
 
-def Main():
-    Menu()
-Main()
+    for archivo in mp4:
+        nombreMp4 = os.path.basename(archivo)
+        nombreMp3 = nombreMp4.replace(".mp4", ".mp3")
+        convertir = 'ffmpeg -i "' + nombreMp4 + '" -y "' + nombreMp3 + '"'
+        print(convertir)
+        os.system(convertir)
+        os.remove(nombreMp4)
+        mover(nombreMp3)
+
+def mover(nombre):
+    print('sexo')
+    shutil.move(nombre, "mp3/")
+    main()
+
+def main():
+    menu()
+main()
--- readme.md
+++ readme.md
@@ -1,9 +1,10 @@
 # Descarga musica de Youtube 
-Un peque;o proyecto donde con la ayuda de ChatGPT escribe  este codgio usando la libreria de pytube de python.
-Puedes agregar varios links y descargarlos.
+Un pequeño proyecto donde con la ayuda de ChatGPT escribe  este codgio usando la libreria de pytube de python.
+Puedes agregar varios links y descargarlos. 
+Actualizacion ya no se puede descargar varios al mismo tiempo
 
 Dependencias
-
+- [FFmpeg](https://ffmpeg.org/)
 ```
 pip install pytube
 ```
